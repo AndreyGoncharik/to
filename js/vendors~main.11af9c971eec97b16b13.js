c043da6df61de042ecb57484819ed1f7ac61cd6a
Goncharik Andrey <goncharikandrey@gmail.com>
2021-07-31 22:10:10 +0300

    menu, add new el

---
+++ js/vendors~main.11af9c971eec97b16b13.js
@@ -0,0 +1,17 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["vendors~main"],{
+
+/***/ "../node_modules/normalize.css/normalize.css":
+/*!***************************************************!*\
+  !*** ../node_modules/normalize.css/normalize.css ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(false) { var cssReload; }
+  
+
+/***/ })
+
+}]);
+//# sourceMappingURL=vendors~main.11af9c971eec97b16b13.js.map
