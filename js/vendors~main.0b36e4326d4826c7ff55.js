c558c82bc8eaead42b3cffb59a529d0db400199e
Goncharik Andrey <goncharikandrey@gmail.com>
2021-08-04 02:10:28 +0300

    menu

---
+++ js/vendors~main.0b36e4326d4826c7ff55.js
@@ -0,0 +1,17 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["vendors~main"],{
+
+/***/ "../node_modules/normalize.css/normalize.css":
+/*!***************************************************!*\
+  !*** ../node_modules/normalize.css/normalize.css ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(false) { var cssReload; }
+  
+
+/***/ })
+
+}]);
+//# sourceMappingURL=vendors~main.0b36e4326d4826c7ff55.js.map
