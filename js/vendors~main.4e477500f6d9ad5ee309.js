08e59da6bcf27c705a95fa9f549022e9d231fb80
Goncharik Andrey <goncharikandrey@gmail.com>
2021-08-05 23:56:12 +0300

    add new item, rem, clean and ect

---
+++ js/vendors~main.4e477500f6d9ad5ee309.js
@@ -0,0 +1,17 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["vendors~main"],{
+
+/***/ "../node_modules/normalize.css/normalize.css":
+/*!***************************************************!*\
+  !*** ../node_modules/normalize.css/normalize.css ***!
+  \***************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(false) { var cssReload; }
+  
+
+/***/ })
+
+}]);
+//# sourceMappingURL=vendors~main.4e477500f6d9ad5ee309.js.map
